c17f12c79a9c816fa492b954661f6e0bf58fcacf
Stefano Bragaglia <stefano.bragaglia@gmail.com>
2020-02-21 15:12:31 +0000

    Test generation of new graphs

--- src/main/python/grammar.py
+++ src/main/python/grammar.py
@@ -1,5 +1,8 @@
-from collections import Counter
+import itertools
+from copy import deepcopy
+from json import dumps
 from typing import Any
+from typing import Dict
 
 from arpeggio import OneOrMore
 from arpeggio import Optional
@@ -52,7 +55,7 @@ def match_character_class():
 
 
 def match_character():
-    return char
+    return character
 
 
 # Character Classes
@@ -66,7 +69,8 @@ def character_group_negative_modifier():
 
 def character_group_item():
     return [
-        character_class, character_class_from_unicode_category, character_range, char_excluding_closed_square_bracket]
+        character_class, character_class_from_unicode_category, character_range,
+        character_excluding_closed_square_bracket]
 
 
 def character_class():
@@ -100,7 +104,7 @@ def unicode_category_name():
 
 
 def character_range():
-    return char, Optional('-', char)
+    return character, Optional('-', character)
 
 
 # Quantifiers
@@ -194,7 +198,7 @@ def letters():
     return RegExMatch(r'[a-zA-Z]+')
 
 
-def char():
+def character():
     # return [RegExMatch(r'\#x9'), RegExMatch(r'\#xA'), RegExMatch(r'\#xD'), RegExMatch(r'[\#x20-\#xD7FF]'),
     #         RegExMatch(r'[\#xE000-\#xFFFD]'), RegExMatch(r'[\#x10000-\#x10FFFF]')]
     return [RegExMatch(r'\u0009'), RegExMatch('\u000A'), RegExMatch('\u000D'),
@@ -203,31 +207,47 @@ def char():
             ]
 
 
-def char_excluding_closed_square_bracket():
+def character_excluding_closed_square_bracket():
     return [RegExMatch(r'\u0009'), RegExMatch(r'\u000A'), RegExMatch(r'\u000D'),
             RegExMatch(r'[\u0020-\u005C]'), RegExMatch(r'[\u005E-\uD7FF]'), RegExMatch(r'[\uE000-\uFFFD]'),
             # RegExMatch(r'[\u10000-\u10FFFF]')
             ]
 
 
-counter = Counter()
-
-
 # noinspection PyMethodMayBeStatic
 class RegExVisitor(PTNodeVisitor):
 
     def visit_regex(self, node, children) -> Any:
+        if len(children) == 1:
+            return children[0]
+
+        # TODO else probably don't needed?
         return node
 
     def visit_expression(self, node, children) -> Any:
+        if len(children) == 1:
+            return children[0]
+
+        # TODO else start/end anchors as well?
         return node
 
     # Anything that can be on one side of the alternation.
     def visit_subexpression(self, node, children) -> Any:
-        return node
+        graph = create_node(
+            'sequence',
+            fontname=ITALIC,
+            shape=BOX,
+            style=ROUNDED,
+        )
+        ident = graph['top']
+        for child in children:
+            graph = create_edge(ident, child['top'], graph=merge(graph, child))
+        graph['top'] = ident
+
+        return graph
 
     def visit_subexpression_item(self, node, children) -> Any:
-        return node
+        return children[0]
 
     # Grouping Constructs
     def visit_group(self, node, children) -> Any:
@@ -238,13 +258,20 @@ class RegExVisitor(PTNodeVisitor):
 
     # Match
     def visit_match(self, node, children) -> Any:
-        if len(children) < 2:
-            return node
+        graph = create_node(
+            'atom',
+            fontname=ITALIC,
+            shape=ELLIPSE,
+            graph=empty(),
+        ) if len(children) == 1 else children[1]
+        ident = graph['top']
+        graph = create_edge(ident, children[0]['top'], graph=merge(graph, children[0]))
+        graph['top'] = ident
 
-        return node
+        return graph
 
     def visit_match_item(self, node, children) -> Any:
-        return node
+        return children[0]
 
     def visit_match_any_character(self, node, children) -> Any:
         return node
@@ -253,7 +280,11 @@ class RegExVisitor(PTNodeVisitor):
         return node
 
     def visit_match_character(self, node, children) -> Any:
-        return node
+        return create_node(
+            dumps(children[0]),
+            shape=BOX,
+            graph=empty(),
+        )
 
     # Character Classes
     def visit_character_group(self, node, children) -> Any:
@@ -291,31 +322,74 @@ class RegExVisitor(PTNodeVisitor):
 
     # Quantifiers
     def visit_quantifier(self, node, children) -> Any:
-        return node
+        return children[0]
 
     def visit_quantifier_type(self, node, children) -> Any:
-        return node
+        return children[0]
 
     def visit_lazy_modifier(self, node, children) -> Any:
         return node
 
     def visit_zero_or_more_quantifier(self, node, children) -> Any:
-        return node
+        graph = create_node(
+            'atom*',
+            fontname=ITALIC,
+            shape=ELLIPSE,
+            style=DASHED,
+            graph=empty(),
+        )
+        return create_edge(
+            graph['top'],
+            graph['top'],
+            label='*',
+            shape=DOT,
+            color=RED,
+            graph=graph,
+        )
 
     def visit_one_or_more_quantifier(self, node, children) -> Any:
-        return node
+        graph = create_node(
+            'atom+',
+            fontname=ITALIC,
+            shape=ELLIPSE,
+            graph=empty(),
+        )
+
+        return create_edge(
+            graph['top'],
+            graph['top'],
+            label='+',
+            shape=DOT,
+            color=RED,
+            graph=graph,
+        )
 
     def visit_zero_or_one_quantifier(self, node, children) -> Any:
-        return '?'
+        return create_node(
+            'atom?',
+            fontname=ITALIC,
+            shape=ELLIPSE,
+            style=DASHED,
+            graph=empty(),
+        )
 
     def visit_range_quantifier(self, node, children) -> Any:
-        return node
+        graph = create_node(
+            f"atom{{{','.join(children)}}}",
+            fontname=ITALIC,
+            shape=ELLIPSE,
+            style=DASHED if int(children[0]) < 1 else None,
+            graph=empty(),
+        )
 
-    def visit_range_quantifier_lower_bound(self, node, children) -> Any:
-        return node
-
-    def visit_range_quantifier_upper_bound(self, node, children) -> Any:
-        return node
+        return create_edge(
+            graph['top'],
+            graph['top'],
+            f"{{{','.join(children)}}}",
+            shape=DOT if max(int(c) for c in children) else None,
+            color=RED if max(int(c) for c in children) else None,
+            graph=graph,
+        )
 
     # Anchors
     def visit_start_of_string_anchor(self, node, children) -> Any:
@@ -356,8 +430,107 @@ class RegExVisitor(PTNodeVisitor):
     def visit_letters(self, node, children) -> Any:
         return node
 
-    def visit_char(self, node, children) -> Any:
-        return node
 
-    def visit_char_excluding_closed_square_bracket(self, node, children) -> Any:
-        return node
+ITALIC = 'times italic'
+BOX = 'box'
+ELLIPSE = 'ellipse'
+DASHED = 'dashed'
+DOT = 'dot'
+ROUNDED = 'rounded'
+RED = 'red'
+
+counter = itertools.count()
+
+
+def empty() -> Dict:
+    return {'top': None, 'nodes': {}, 'edges': {}}
+
+
+def create_node(
+        label: str,
+        fontname: str = None,
+        shape: str = None,
+        style: str = None,
+        color: str = None,
+        graph: Dict = None,
+) -> Dict:
+    if graph is None:
+        graph = empty()
+
+    ident = next(counter)
+    graph['top'] = ident
+    node = graph['nodes'].setdefault(ident, {'label': label})
+    if fontname:
+        node['fontname'] = fontname
+    if shape:
+        node['shape'] = shape
+    if style:
+        node['style'] = style
+    if color:
+        node['color'] = color
+
+    return graph
+
+
+def create_edge(
+        source: int,
+        target: int,
+        label: str = None,
+        shape: str = None,
+        color: str = None,
+        graph: Dict = None,
+) -> Dict:
+    if graph is None:
+        graph = empty()
+
+    edge = graph['edges'].setdefault(source, {}).setdefault(target, {})
+    if label:
+        edge['label'] = label
+    if shape:
+        edge['shape'] = shape
+    if color:
+        edge['color'] = color
+    if graph['top'] == target:
+        graph['top'] = source
+
+    return graph
+
+
+def merge(graph1: Dict, graph2: Dict) -> Dict:
+    nodes = deepcopy(graph1['nodes'])
+    for ident, node in graph2['nodes'].items():
+        nodes[ident] = node
+    edges = deepcopy(graph1['edges'])
+    for source, tail in graph2['edges'].items():
+        for target, content in tail.items():
+            edge = edges.setdefault(source, dict()).setdefault(target, dict())
+            for key, value in content.items():
+                edge[key] = value
+
+    return {'top': max(i for i in (graph1['top'], graph2['top']) if i is not None), 'nodes': nodes, 'edges': edges}
+
+
+def convert(graph: Dict) -> str:
+    nodes = []
+    for ident, node in graph.get('nodes', dict()).items():
+        params = ' '.join(f'{l}={dumps(v)}' for l, v in node.items())
+        nodes.append(f"\t{ident} [{params}];")
+    nodes = '\n'.join(nodes)
+    if nodes:
+        nodes += '\n'
+
+    space = '\n' if graph['nodes'] and graph['edges'] else ''
+
+    edges = []
+    for source, adjacency in graph.get('edges', dict()).items():
+        for target, edge in adjacency.items():
+            if edge:
+                params = ' '.join(f'{l}={dumps(v)}' for l, v in edge.items())
+                edges.append(f"\t{source} -> {target} [{params}];")
+            else:
+                edges.append(f"\t{source} -> {target};")
+    edges = '\n'.join(edges)
+    if edges:
+        edges += '\n'
+
+    return f"""digraph regex_graph {{\n{nodes}{space}{edges}\n\tfontname="times"\n}}\n"""
--- src/main/scripts/dotregex.py
+++ src/main/scripts/dotregex.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
+import json
 import os
 import re
 
 from arpeggio import ParserPython
 from arpeggio import visit_parse_tree
-from grammar import RegExVisitor
 
+from grammar import convert
 from grammar import regex
+from grammar import RegExVisitor
 
 if __name__ == '__main__':
     parser = ParserPython(regex, ws='\t ', debug=True)
     os.system(f'dot -Tpng -oregex_model.png regex_parser_model.dot')
     for test_expr in (
             # "abc?",
-            # "ab+c",
+            "ab+c",
             # "(ab)+c",
             # "[^-a\\-f-z\"\\]aaaa-]?",
             # "abc|d",
             # "a?",
             # ".*?(a|b){,9}?",
             # "(XYZ)|(123)",
-            "a",
     ):
         print(test_expr)
         print('-' * len(test_expr))
@@ -30,7 +31,9 @@ if __name__ == '__main__':
             print(e)
         else:
             result = visit_parse_tree(parse_tree, RegExVisitor(debug=True))
-            print(result)
+
+            print(convert(result))
+
             safe = re.sub(r'\W', '_', test_expr)
             os.system(f'dot -Tpng -o{safe}.png regex_parse_tree.dot')
         print('\n' * 5)
