70861cacb000824ce9c1099a3cdb34373f617c8e
Stefano Bragaglia <stefano.bragaglia@gmail.com>
2020-02-27 15:31:58 +0000

    Simplify charsets (naive)

--- src/main/scripts/dotregex.py
+++ src/main/scripts/dotregex.py
@@ -12,16 +12,16 @@ from visitor import RegExVisitor
 if __name__ == '__main__':
     parser = ParserPython(regex, ws='\t ', debug=False)
     for test_expr in (
-            # "^(abc)?\\1$",
+            "^(abc)?\\1$",
             "[^]\\p{L}\\n\\x64-\\x65\\u0066-\\u0067a-bc]+",
-            # ".?a*\\w+\\x64{2}\\u0064{0,3}",
-            # ".??a*?\\w+?\\x64{2}?\\u0064{0,3}?",
-            # "abc?",
-            # "ab+c",
-            # "(ab)+c",
-            # "abc|d",
-            # ".*?(a|b){0,9}?",
-            # "(XYZ)|(123)",
+            ".?a*\\w+\\x64{2}\\u0064{0,3}",
+            ".??a*?\\w+?\\x64{2}?\\u0064{0,3}?",
+            "abc?",
+            "ab+c",
+            "(ab)+c",
+            "abc|d",
+            ".*?(a|b){0,9}?",
+            "(XYZ)|(123)",
             # # "[^-a\\-f-z\"\\]aaaa-]?",
     ):
         print(test_expr)
